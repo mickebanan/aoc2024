ba4f05e2ac9f10e633ef8d9e4a9a6ddbee32ed12
mickebanan <mikael.molsa@proton.me>
2024-12-02 14:23:47 +0200

    Add helpers.timer.

--- 1.py
+++ 1.py
@@ -1,3 +1,5 @@
+import helpers
+
 data = """
 3   4
 4   3
@@ -10,10 +12,14 @@ values = [tuple(row.split()) for row in data if row]
 first = sorted(int(t[0]) for t in values)
 second1 = sorted(int(t[1]) for t in values)
 second2 = second1.copy()
-s1 = s2 = 0
-for v1 in first:
-    v2 = second1.pop(0)
-    s1 += abs(v1 - v2)
-    score = len([v2 for v2 in second2 if v2 == v1])
-    s2 += v1 * score
-print(s1, s2)
+
+@helpers.timer
+def solve():
+    s1 = s2 = 0
+    for v1 in first:
+        v2 = second1.pop(0)
+        s1 += abs(v1 - v2)
+        score = len([v2 for v2 in second2 if v2 == v1])
+        s2 += v1 * score
+    print(s1, s2)
+solve()
