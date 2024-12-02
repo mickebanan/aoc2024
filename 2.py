631bdb608524718d660a6e8514c9b1e7b670b67e
mickebanan <mikael.molsa@proton.me>
2024-12-02 14:24:37 +0200

    Untrack input files.

---
+++ 2.py
@@ -0,0 +1,44 @@
+import itertools
+
+import helpers
+
+data = """7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9""".split('\n')
+data = open('2.dat').readlines()
+
+def check_values(values):
+    first = values[0]
+    potential_errors = set()
+    for i, (v1, v2) in enumerate(itertools.pairwise(values)):
+        if v1 < v2 and 1 <= (v2 - v1) <= 3 and first < v2:
+            pass
+        elif v1 > v2 and 1 <= (v1 - v2) <= 3 and first > v2:
+            pass
+        else:
+            potential_errors |= {i - 1, i, i + 1}
+    if potential_errors:
+        return potential_errors
+    if values == sorted(values) or values == sorted(values, reverse=True):
+        return True
+    return False
+
+@helpers.timer
+def solve():
+    s1 = s2 = 0
+    for row in data:
+        values = [int(v) for v in row.split()]
+        ret = check_values(values)
+        if ret is True:
+            s1 += 1
+        else:
+            for v in sorted(ret):
+                vals = values[:v] + values[v+1:]
+                if check_values(vals) is True:
+                    s2 += 1
+                    break
+    print(s1, s1 + s2)
+solve()
